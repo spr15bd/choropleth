d91910e246ad307091b811c1ad2437033d4158fd
spr15bd <bevandady@gmail.com>
2021-04-21 21:19:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ loadAndProcessData().then(counties=> {
 
 	colorLegend.append("g")
       		.attr("id", "legend-axis")
-      		.attr("transform", "translate("+0+", "+(30)+")")
+      		.attr("transform", "translate("+0+", "-(60)+")")
       		.call(legendAxis); 
 
 });
